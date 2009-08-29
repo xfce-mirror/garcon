2caae8003775ce0c521538f03240394bc0785a4f
Nick Schermer <nick@xfce.org>
2009-08-29 17:40:23 +0200

    Add a private for some internal functions.

---
+++ garcon/garcon-private.h
@@ -0,0 +1,36 @@
+/*-
+ * vi:set et ai sts=2 sw=2 cindent:
+ *
+ * Copyright (c) 2009 Nick Schermer <nick@xfce.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#if !defined(GARCON_INSIDE_GARCON_H) && !defined(GARCON_COMPILATION)
+#error "Only <garcon/garcon.h> can be included directly. This file may disappear or change contents."
+#endif
+
+#ifndef __GARCON_PRIVATE_H__
+#define __GARCON_PRIVATE_H__
+
+G_BEGIN_DECLS
+
+gboolean _garcon_str_is_equal (const gchar *a,
+                               const gchar *b);
+
+G_END_DECLS
+
+#endif /* !__GARCON_PRIVATE_H__ */
