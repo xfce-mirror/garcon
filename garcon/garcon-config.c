xfce-mirror/garcon

commit 799e206b6780ee6bab2bb56debf8412dca0346f5

Also try the garcon install sysconfigdir for config lookups.
Hard-code the $(sysconfdir)/xdg path in garcon_config_lookup(),
so the chance we find xfce-applications.menu is higher on
systems that do not have XDG_CONFIG_DIRS setup properly.

garcon/garcon-config.c
@@ -169,7 +169,7 @@ garcon_config_lookup (const gchar *filename)
   path = g_build_filename (g_get_user_config_dir (), filename, NULL);
   if (g_path_is_absolute (path) && g_file_test (path, G_FILE_TEST_IS_REGULAR))
     return path;
-    
+
   g_free (path);
   path = NULL;
 
@@ -185,6 +185,15 @@ garcon_config_lookup (const gchar *filename)
           path = NULL;
         }
     }
+
+  /* Also try the install prefix of garcon in case XDG_CONFIG_DIRS is not
+   * properly set (startxfce4 for exaple should take care of that) */
+  path = g_build_filename (SYSCONFIGDIR, filename, NULL);
+  if (g_path_is_absolute (path) && g_file_test (path, G_FILE_TEST_IS_REGULAR))
+    return path;
+
+  g_free (path);
+  path = NULL;
 
   /* Return the path or NULL if the file could not be found */
   return path;

garcon/Makefile.am
@@ -22,6 +22,7 @@ INCLUDES = 								\
 	-DGARCON_COMPILATION						\
 	-DGARCON_VERSION_API=\"$(GARCON_VERSION_API)\"			\
 	-DG_LOG_DOMAIN=\"garcon\"					\
+	-DSYSCONFIGDIR=\"$(sysconfdir)/xdg\"				\
 	$(PLATFORM_CFLAGS)
 
 lib_LTLIBRARIES = 							\

commit 62ec543886a724d1956bacf0887e0faa2ec011e3

Respect XDG priorities when returning the menu file (Bug #7470)
799e206b introduced a fallback that is supposed to try
SYSCONFIGDIR/menus/xfce-applications.menu when no menu file was found in
$XDG_CONFIG_DIRS. However, this doesn't work as expected, as
SYSCONFIGDIR/menus/xfce-applications.menu is always returned instead, and
overrides any menu in $XDG_CONFIG_DIRS.

garcon/garcon-config.c
@@ -179,11 +179,11 @@ garcon_config_lookup (const gchar *filename)
     {
       /* Build the filename, if the file exists return the path */
       path = g_build_filename (dirs[i], filename, NULL);
-      if (!g_path_is_absolute (path) || !g_file_test (path, G_FILE_TEST_IS_REGULAR))
-        {
-          g_free (path);
-          path = NULL;
-        }
+      if (g_path_is_absolute (path) && g_file_test (path, G_FILE_TEST_IS_REGULAR))
+        return path;
+
+      g_free (path);
+      path = NULL;
     }
 
   /* Also try the install prefix of garcon in case XDG_CONFIG_DIRS is not