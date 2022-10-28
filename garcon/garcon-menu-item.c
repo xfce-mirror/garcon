xfce-mirror/garcon

commit f33c23d2b1e09b9a461cc7272cdb029afa9669ab

garcon: Escape '%' when building command from URL desktop files
Since Garcon reduces the case of URL desktop files to the case of a
classic file with an Exec key of value `exo-open 'URL'`, and there is no
reliable way to distinguish the two cases from the outside when invoking
`garcon_menu_item_get_command()`, Garcon must escape the '%' characters
in the URL as if they appeared in the value of an Exec key.

Fixes: #31

garcon/garcon-menu-item.c
@@ -728,13 +728,19 @@ garcon_menu_item_lists_equal (GList *list1,
 static gchar *
 garcon_menu_item_url_exec (XfceRc *rc)
 {
+  GString     *string;
   const gchar *url;
   gchar       *url_exec = NULL;
 
   /* Support Type=Link items */
   url = xfce_rc_read_entry_untranslated (rc, G_KEY_FILE_DESKTOP_KEY_URL, NULL);
   if (url != NULL)
-    url_exec = g_strdup_printf ("exo-open '%s'", url);
+    {
+      string = g_string_new (url);
+      g_string_replace (string, "%", "%%", 0);
+      url_exec = g_strdup_printf ("exo-open '%s'", string->str);
+      g_string_free (string, TRUE);
+    }
 
   return url_exec;
 }

commit 6bbe0300e87e1e8b8244d66e74b18efbcefdc295

garcon: Fix previous commit
Only GLib 2.66 will be required for Xfce 4.18.

Fixes: f33c23d2b1e09b9a461cc7272cdb029afa9669ab

garcon/garcon-menu-item.c
@@ -736,8 +736,20 @@ garcon_menu_item_url_exec (XfceRc *rc)
   url = xfce_rc_read_entry_untranslated (rc, G_KEY_FILE_DESKTOP_KEY_URL, NULL);
   if (url != NULL)
     {
+#if GLIB_CHECK_VERSION (2, 68, 0)
       string = g_string_new (url);
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
       g_string_replace (string, "%", "%%", 0);
+G_GNUC_END_IGNORE_DEPRECATIONS
+#else
+      string = g_string_sized_new (2 * strlen (url));
+      for (const gchar *p = url; *p != '\0'; p++)
+        if (*p == '%')
+          string = g_string_append (string, "%%");
+        else
+          string = g_string_append_c (string, *p);
+      string = g_string_append_c (string, '\0');
+#endif
       url_exec = g_strdup_printf ("exo-open '%s'", string->str);
       g_string_free (string, TRUE);
     }