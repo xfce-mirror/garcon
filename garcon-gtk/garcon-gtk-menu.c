xfce-mirror/garcon

commit 141c397f1dd19664556a5c09a056fdd1e15de62e

garcon-gtk: Prevent use-after-free when loading garcon menu
In a few rare cases (for example, when reloading panel configuration via
xfce4-panel-profiles, but this is difficult to reproduce), garcon menu
loading may start during its finalization, after the lock added in
84fa5fa6. To avoid this, we cancel loading on dispose (i.e. before
finalize) via a weak reference.

Fixes: 966f7c99dc5ebf39ec423883652b805b749a9a2d
Related: !18

garcon-gtk/garcon-gtk-menu.c
@@ -619,7 +619,11 @@ garcon_gtk_menu_load_async (GTask        *task,
 
   g_mutex_lock (&menu->priv->load_lock);
 
-  menu->priv->is_loaded = garcon_menu_load (menu->priv->menu, cancellable, &error);
+  if (g_cancellable_set_error_if_cancelled (cancellable, &error))
+    menu->priv->is_loaded = FALSE;
+  else
+    menu->priv->is_loaded = garcon_menu_load (menu->priv->menu, cancellable, &error);
+
   if (! menu->priv->is_loaded)
     g_task_return_error (task, error);
 
@@ -1091,6 +1095,16 @@ garcon_gtk_menu_new (GarconMenu *garcon_menu)
 
 
 
+static void
+garcon_gtk_menu_load_cancel (gpointer data,
+                             GObject *garcon_menu)
+{
+  GarconGtkMenu *menu = data;
+  g_cancellable_cancel (menu->priv->load_cancel);
+}
+
+
+
 /**
  * garcon_gtk_menu_set_menu:
  * @menu  : A #GarconGtkMenu
@@ -1116,6 +1130,7 @@ garcon_gtk_menu_set_menu (GarconGtkMenu *menu,
   if (garcon_menu != NULL)
     {
       menu->priv->menu = g_object_ref (garcon_menu);
+      g_object_weak_ref (G_OBJECT (menu->priv->menu), garcon_gtk_menu_load_cancel, menu);
       g_signal_connect_object (menu->priv->menu, "reload-required",
                                G_CALLBACK (garcon_gtk_menu_reload), menu, G_CONNECT_SWAPPED);
     }

commit 432f87c344c34cc3bdc991478f556dee8fb410f0

garcon-gtk: Remove weak ref on GarconMenu on finalize()
Can cause segfault if GarconGtkMenu is finalized first.

Fixes: 141c397f1dd19664556a5c09a056fdd1e15de62e

garcon-gtk/garcon-gtk-menu.c
@@ -69,6 +69,8 @@ static void                 garcon_gtk_menu_add                         (GarconG
                                                                          GarconMenu              *garcon_menu);
 static void                 garcon_gtk_menu_load                        (GarconGtkMenu           *menu);
 static void                 garcon_gtk_menu_reload                      (GarconGtkMenu           *menu);
+static void                 garcon_gtk_menu_load_cancel                 (gpointer                 data,
+                                                                         GObject                 *garcon_menu);
 
 
 
@@ -240,7 +242,10 @@ garcon_gtk_menu_finalize (GObject *object)
 
   /* Release menu */
   if (menu->priv->menu != NULL)
-    g_object_unref (menu->priv->menu);
+    {
+      g_object_weak_unref (G_OBJECT (menu->priv->menu), garcon_gtk_menu_load_cancel, menu);
+      g_object_unref (menu->priv->menu);
+    }
 
   (*G_OBJECT_CLASS (garcon_gtk_menu_parent_class)->finalize) (object);
 }